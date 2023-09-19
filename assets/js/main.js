4df0bd2bd2d3a8aeadea42a1eba02ae63ad940f9
TGustavS <tjark@bbsmkm.de>
2023-09-19 11:47:32 +0200

    switched assets to check if mobile works

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,81 +4,80 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function ($) {
-  var $window = $(window),
-    $body = $("body"),
-    $wrapper = $("#page-wrapper"),
-    $banner = $("#banner"),
-    $header = $("#header");
+(function($) {
 
-  // Breakpoints.
-  breakpoints({
-    xlarge: ["1281px", "1680px"],
-    large: ["981px", "1280px"],
-    medium: ["737px", "980px"],
-    small: ["481px", "736px"],
-    xsmall: [null, "480px"],
-  });
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#page-wrapper'),
+		$banner = $('#banner'),
+		$header = $('#header');
 
-  // Play initial animations on page load.
-  $window.on("load", function () {
-    window.setTimeout(function () {
-      $body.removeClass("is-preload");
-    }, 100);
-  });
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ]
+		});
 
-  // Mobile?
-  if (true)
-    //(browser.mobile)
-    $body.addClass("is-mobile");
-  else {
-    breakpoints.on(">medium", function () {
-      $body.removeClass("is-mobile");
-    });
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-    breakpoints.on("<=medium", function () {
-      $body.addClass("is-mobile");
-    });
-  }
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
 
-  // Scrolly.
-  $(".scrolly").scrolly({
-    speed: 1500,
-    offset: $header.outerHeight(),
-  });
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
 
-  // Menu.
-  $("#menu")
-    .append('<a href="#menu" class="close"></a>')
-    .appendTo($body)
-    .panel({
-      delay: 500,
-      hideOnClick: true,
-      hideOnSwipe: true,
-      resetScroll: true,
-      resetForms: true,
-      side: "right",
-      target: $body,
-      visibleClass: "is-menu-visible",
-    });
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
 
-  // Header.
-  if ($banner.length > 0 && $header.hasClass("alt")) {
-    $window.on("resize", function () {
-      $window.trigger("scroll");
-    });
+		}
 
-    $banner.scrollex({
-      bottom: $header.outerHeight() + 1,
-      terminate: function () {
-        $header.removeClass("alt");
-      },
-      enter: function () {
-        $header.addClass("alt");
-      },
-      leave: function () {
-        $header.removeClass("alt");
-      },
-    });
-  }
+	// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				speed: 1500,
+				offset: $header.outerHeight()
+			});
+
+	// Menu.
+		$('#menu')
+			.append('<a href="#menu" class="close"></a>')
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'is-menu-visible'
+			});
+
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
+
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$banner.scrollex({
+				bottom:		$header.outerHeight() + 1,
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
+
+		}
+
 })(jQuery);
